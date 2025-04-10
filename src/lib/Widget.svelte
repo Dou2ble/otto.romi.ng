aabe673355d61e30aa789a3956db6068595fd44a
Dou2ble <22otro@stockholmscience.se>
2025-04-10 22:48:40 +0200

    initial commit

---
+++ src/lib/Widget.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div class="flex justify-center items-center roundned-md bg-gray-800">
+	{@render children()}
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import "../app.css";
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div class="flex justify-center items-center bg-gray-600 h-lvh">
+	{@render children()}
+</div>
